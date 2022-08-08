9fdbcbc50d933da7a1ca5639b637632aee8a22f4
Tracy <okorietracy8@gmail.com>
2022-08-08 08:05:01 -0700

    Printf task

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * struct printer - structure for printing to stdout, given a specifier
+ * @specifier: the specifier recognized by this printer (without the %)
+ * @run: the function to run when this printer is invoked
+ */
+typedef struct printer
+{
+	char *specifier;
+	int (*run)(va_list);
+} printer;
+
+printer _get_printer(const char *specifier);
+int _putchar(char c);
+int _printf(const char *format, ...);
+int _print_char(va_list);
+int _print_str(va_list);
+int _print_decimal(va_list);
+int _print_int(va_list);
+
+#endif /* MAIN_H */
