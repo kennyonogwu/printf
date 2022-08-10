93407eb4cbd5c5345f44ed414190299af3711f32
kennyonogwu <99299864+kennyonogwu@users.noreply.github.com>
2022-08-10 18:52:55 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -64,21 +64,6 @@ void print_buffer(char buffer[], int *buff_ind)
 
 	*buff_ind = 0;
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About#include "main.h"
-
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
